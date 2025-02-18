95b91581fa0657503475ddba101c23d3548943ad
GuiluilhermeMori <guilhermebmori@hotmail.com>
2025-02-18 18:28:11 -0300

    create db:seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cadastrando moedas...."
+
+Coin.create!(
+  description: "Bitcoin", 
+  acronym: "BTC",
+  url_image: "https://img.icons8.com/?size=512&id=63192&format=png"
+)
+Coin.create!(
+  description: "Ethereum", 
+  acronym: "ETH",
+  url_image: "https://cryptologos.cc/logos/ethereum-eth-logo.png"
+)
+Coin.create!(
+  description: "Dash", 
+  acronym: "DASH",
+  url_image: "https://cdn-icons-png.flaticon.com/512/825/825534.png"
+)
+
+puts "Moedas cadastradas com sucesso!" 
