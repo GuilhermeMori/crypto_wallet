3d3a3f73951b1dd7aeb35bf6daae1babb0b6a39f
GuiluilhermeMori <guilhermebmori@hotmail.com>
2025-02-19 08:12:30 -0300

    refactor db:seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-puts "Cadastrando moedas...."
+spinner = TTY::Spinner.new("[:spinner]Cadastrando Moedas...")
+spinner.auto_spin
 
-Coin.create!(
-  description: "Bitcoin", 
-  acronym: "BTC",
-  url_image: "https://img.icons8.com/?size=512&id=63192&format=png"
-)
-Coin.create!(
-  description: "Ethereum", 
-  acronym: "ETH",
-  url_image: "https://cryptologos.cc/logos/ethereum-eth-logo.png"
-)
-Coin.create!(
-  description: "Dash", 
-  acronym: "DASH",
-  url_image: "https://cdn-icons-png.flaticon.com/512/825/825534.png"
-)
+coins = 
+[
+  { description: "Bitcoin", 
+    acronym: "BTC",
+    url_image: "https://img.icons8.com/?size=512&id=63192&format=png"
+  },
+  { description: "Ethereum", 
+    acronym: "ETH",
+    url_image: "https://cryptologos.cc/logos/ethereum-eth-logo.png"
+  },
+  {
+    description: "Dash", 
+    acronym: "DASH",
+    url_image: "https://cdn-icons-png.flaticon.com/512/825/825534.png"
+  }
+]
 
-puts "Moedas cadastradas com sucesso!" 
+coins.each do |coin|
+  Coin.find_or_create_by!(coin)
+end
+
+spinner.success("Concluido!")
--- lib/tasks/dev.rake
+++ lib/tasks/dev.rake
@@ -11,6 +11,8 @@ namespace :dev do
     end
   end
 
+  private 
+  
   def show_spinner(msg_start,msg_end= "Concluido!")
     spinner = TTY::Spinner.new("[:spinner]#{msg_start}")
     spinner.auto_spin
