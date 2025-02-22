4f44ca22c8ab82085a9329ece2a71dc643858ee1
GuiluilhermeMori <guilhermebmori@hotmail.com>
2025-02-22 20:47:36 -0300

    using i18n

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,6 @@
 module ApplicationHelper
-  def locale(locale)
-    if I18n.locale == :en
-    else
-      "Português do Brasil"
-      end
+  def locale
+    I18n.locale == :en ? "Estados Unidos": "Português do Brasil"
   end
   def data_br(data_us)
     data_us.strftime("%d/%m/%Y")
