92444e746f52778b2e67b7d8ad8b487f649157d6
GuiluilhermeMori <guilhermebmori@hotmail.com>
2025-02-22 20:11:41 -0300

    add gem i18n

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,10 @@
 module ApplicationHelper
+  def locale(locale)
+    if I18n.locale == :en
+    else
+      "Português do Brasil"
+      end
+  end
   def data_br(data_us)
     data_us.strftime("%d/%m/%Y")
   end
